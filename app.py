1703482f624acd169796c821be291d6949cf1f2a
admin <itaebread@gmail.com>
2021-06-09 10:35:51 +0900

    fix userdb for like

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 SECRET_KEY = 'HANGHAE'
 
+
 # 회원가입
 @app.route('/join/save', methods=['POST'])
 def sign_up():
@@ -22,18 +23,70 @@ def sign_up():
         "userid": userid_receive,
         "password": password_hash,
         "username": username_receive,
+        "userlikelist": {
+            "0": 0,
+            "1": 0,
+            "2": 0,
+            "3": 0,
+            "4": 0,
+            "5": 0,
+            "6": 0,
+            "7": 0,
+            "8": 0,
+            "9": 0,
+            "10": 0,
+            "11": 0,
+            "12": 0,
+            "13": 0,
+            "14": 0,
+            "15": 0,
+            "16": 0,
+            "17": 0,
+            "18": 0,
+            "19": 0,
+            "20": 0,
+            "21": 0,
+            "22": 0,
+            "23": 0,
+            "24": 0,
+            "25": 0,
+            "26": 0,
+            "27": 0,
+            "28": 0,
+            "29": 0,
+            "30": 0,
+            "31": 0,
+            "32": 0,
+            "33": 0,
+            "34": 0,
+            "35": 0,
+            "36": 0,
+            "37": 0,
+            "38": 0,
+            "39": 0,
+            "40": 0,
+            "41": 0,
+            "42": 0,
+            "43": 0,
+            "44": 0,
+            "45": 0,
+            "46": 0,
+            "47": 0,
+            "48": 0,
+            "49": 0,
+
+        }
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
 
-#회원가입시 아이디중복확인
+# 회원가입시 아이디중복확인
 @app.route('/join/check_dup', methods=['POST'])
 def check_dup():
     userid_receive = request.form['userid_give']
     exists = bool(db.users.find_one({"userid": userid_receive}))
     return jsonify({'result': 'success', 'exists': exists})
-
 
 
 # 로그인
@@ -74,6 +127,7 @@ def session():
         return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
     except jwt.exceptions.DecodeError:
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+
 
 # -- index --#
 @app.route('/')
@@ -125,6 +179,7 @@ def like_wine():
     new_like = current_like + 1
     db.winelist1.update_one({'wine_name': name_receive}, {'$set': {'wine_like': new_like}})
     return jsonify({'msg': '좋아요완료!'})
+
 
 # deletebtn
 @app.route('/api/delete', methods=['POST'])
